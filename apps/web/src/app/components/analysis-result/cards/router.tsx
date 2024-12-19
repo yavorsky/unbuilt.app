f7b0d3840cca6949d760de2288f5cd66ee084330
Artem Yavorskyi <aqson@me.com>
2024-12-19 00:15:51 +0000

    fix: remove unused var

--- apps/web/src/app/components/analysis-result/cards/router.tsx
+++ apps/web/src/app/components/analysis-result/cards/router.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { AnalysisKeys, AnalyzeResult } from '@unbuilt/analyzer';
+import { AnalyzeResult } from '@unbuilt/analyzer';
 import { SingleResultAnalysisCard } from './common/single-result-card';
 import { capitalize } from 'lodash-es';
 import { Calendar } from 'lucide-react';
