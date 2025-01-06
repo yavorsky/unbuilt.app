2ceb8741505a8579057c8fe283f63e4a54731a52
Artem Yavorskyi <aqson@me.com>
2025-01-06 00:15:02 +0000

    fix: load env vars to supabase init file

--- apps/web/src/lib/supabase.ts
+++ apps/web/src/lib/supabase.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
+import '../../envConfig';
 
 if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
   throw new Error('Missing Supabase environment variables');
