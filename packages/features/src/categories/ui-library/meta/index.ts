5eb1859c29b741b63454d12a15504c2fa3034b8f
Artem Yavorskyi <aqson@me.com>
2025-02-06 22:21:59 +0000

    feat: add web-components for ui detection

--- packages/features/src/categories/ui-library/meta/index.ts
+++ packages/features/src/categories/ui-library/meta/index.ts
@@ -8,6 +8,7 @@ import { react } from './react.js';
 import { solid } from './solid.js';
 import { svelte } from './svelte.js';
 import { vue } from './vue.js';
+import { webComponents } from './web-components.js';
 
 export const meta = {
   react,
@@ -20,4 +21,5 @@ export const meta = {
   inferno,
   vue,
   jQuery,
+  webComponents,
 } as const;
