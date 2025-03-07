df074058c5b735f6428321c8aa3fb6ac3badcc5d
Artem Yavorskyi <aqson@me.com>
2025-03-07 14:05:27 +0000

    feat: fix prop names for moment

--- packages/features/src/categories/dates/meta/icons/moment.tsx
+++ packages/features/src/categories/dates/meta/icons/moment.tsx
@@ -15,7 +15,7 @@ const MomentLogo = ({ width = 24, height = 24, className = '' }: LogoProps) => {
       <title>Moment.js</title>
       {/* Outer ring */}
       <ellipse
-        stroke-width="3"
+        strokeWidth="3"
         cx="20"
         cy="20"
         rx="18.5"
@@ -23,7 +23,7 @@ const MomentLogo = ({ width = 24, height = 24, className = '' }: LogoProps) => {
         stroke="#fff"
       ></ellipse>
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         fill="#fff"
         d="M20,36 C28.836556,36 36,28.836556 36,20 C36,11.163444 28.836556,4 20,4 C11.163444,4 4,11.163444 4,20 C4,28.836556 11.163444,36 20,36 Z M20,5 C19.4477153,5 19,5.44994876 19,6.00684547 L19,19 L11.9970301,19 C11.4463856,19 11,19.4438648 11,20 C11,20.5522847 11.4556644,21 11.9953976,21 L21,21 L21,6.00087166 C21,5.4481055 20.5561352,5 20,5 Z"
       ></path>
