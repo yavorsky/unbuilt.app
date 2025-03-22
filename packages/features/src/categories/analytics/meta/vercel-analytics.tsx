813b5238e854b02723b5dcb8660d2bf86fb62158
Artem Yavorskyi <aqson@me.com>
2025-03-22 12:24:38 +0000

    feat: add analytics category

--- packages/features/src/categories/analytics/meta/vercel-analytics.tsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from 'react';
-import { Meta } from '../../../types/meta.js';
-
-export const vercelAnalytics: Meta = {
-  name: 'Vercel Analytics',
-  website: 'https://vercel.com/analytics',
-  description: 'Powerful analytics for every business',
-  Icon: lazy(() => import('./icons/vercel-analytics.jsx')),
-};
