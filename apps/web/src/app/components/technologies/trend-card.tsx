32f9441c45c7b73dcf76d7cdede8d69424eae5c1
Artem Yavorskyi <aqson@me.com>
2025-03-23 16:07:12 +0000

    feat: improve trends

--- apps/web/src/app/components/technologies/trend-card.tsx
+++ apps/web/src/app/components/technologies/trend-card.tsx
@@ -2,6 +2,8 @@ import { Card, CardContent } from '@/components/ui';
 import { ChartConfig } from '@/components/ui/chart';
 import { ArrowDownIcon, ArrowUpIcon } from 'lucide-react';
 import { DisplayType } from './technology-trends';
+import { AnalysisTechnologies } from '@unbuilt/analyzer';
+import { useRouter } from 'next/navigation';
 
 interface TrendInfo {
   name: string;
@@ -14,14 +16,17 @@ export function TrendCard({
   chartConfig,
   displayType,
   className,
+  type,
 }: {
   trend: TrendInfo;
   chartConfig: ChartConfig;
   displayType?: DisplayType;
   className?: string;
+  type?: AnalysisTechnologies;
 }) {
   const Icon = trend.direction === 'up' ? ArrowUpIcon : ArrowDownIcon;
   const label = chartConfig[trend.name]?.label || trend.name;
+  const router = useRouter();
   const colorClass =
     trend.direction === 'up' ? 'text-green-500' : 'text-red-500';
 
@@ -32,7 +37,14 @@ export function TrendCard({
     >
       <CardContent className="p-4">
         <div className="flex items-center justify-center lg:justify-between md:justify-between flex-wrap">
-          <span className="text-xl font-medium text-foreground">{label}</span>
+          <span
+            className="text-xl font-medium text-foreground hover:underline cursor-pointer"
+            onClick={() => {
+              router.push(`/technologies/${type}/${trend.name}`);
+            }}
+          >
+            {label}
+          </span>
           <div className={`flex items-center gap-1 ${colorClass}`}>
             <Icon className="w-4 h-4" />
             <span className="text-base font-medium text-inherit">
