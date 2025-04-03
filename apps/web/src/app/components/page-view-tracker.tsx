8067223bd1cc6fc28dca9358f4e5da8a46d9ec6d
Artem Yavorskyi <aqson@me.com>
2025-04-03 17:21:38 +0000

    fix: page_view shouldnt react on search params change

--- apps/web/src/app/components/page-view-tracker.tsx
+++ apps/web/src/app/components/page-view-tracker.tsx
@@ -1,23 +1,18 @@
 'use client';
 
 import { useEffect } from 'react';
-import { usePathname, useSearchParams } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import { trackPageView } from '../utils/analytics';
 
 export function PageViewTracker() {
   const pathname = usePathname();
-  const searchParams = useSearchParams();
 
   useEffect(() => {
     // Track page view when the path or search params change
     if (pathname) {
-      const url = searchParams?.size
-        ? `${pathname}?${searchParams.toString()}`
-        : pathname;
-
-      trackPageView(url);
+      trackPageView(pathname);
     }
-  }, [pathname, searchParams]);
+  }, [pathname]);
 
   return null; // This component doesn't render anything
 }
