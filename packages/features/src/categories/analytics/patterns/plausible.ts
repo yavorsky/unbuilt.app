8666f25605117b7c07b13f21ecfdf4eeb33e6e94
Artem Yavorskyi <aqson@me.com>
2025-04-09 23:43:49 +0000

    fix: improve plausible script detection

--- packages/features/src/categories/analytics/patterns/plausible.ts
+++ packages/features/src/categories/analytics/patterns/plausible.ts
@@ -15,6 +15,16 @@ export const plausible = [
   },
   {
     name: 'script' as const,
+    score: 1.2,
+    scripts: [
+      /console\.warn\(['"]\[Plausible\] Ignoring event because website is running locally['"]\)/,
+      /console\.warn\(['"]\[Plausible\] Ignoring event because "plausible_ignore" is set to "true" in localStorage['"]\)/,
+      /['"]apiHost["']:\s*["']https:\/\/plausible\.io["']/,
+      /window\.localStorage\.plausible_ignore === ['"]true['"]/,
+    ],
+  },
+  {
+    name: 'scriptDataDomain' as const,
     score: 1,
     browser: async (page: Page) => {
       return page.evaluate(() => {
