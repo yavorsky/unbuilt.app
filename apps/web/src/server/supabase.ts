88aa5fc0835d47736ddd789283b47b68976566df
Artem Yavorskyi <aqson@me.com>
2025-04-15 23:48:54 +0000

    fix: skip pr build for next.js app since its build with vercel

--- apps/web/src/server/supabase.ts
+++ apps/web/src/server/supabase.ts
@@ -2,11 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 import '../../envConfig';
 import { Database } from '../../supabase/database.types';
 
-if (!process.env.SUPABASE_URL || !process.env.SUPABASE_KEY) {
-  throw new Error('Missing Supabase environment variables');
-}
-
 export const supabase = createClient<Database>(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_KEY!
 );
