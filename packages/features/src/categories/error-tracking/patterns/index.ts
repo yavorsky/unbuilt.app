a6c591d46795f69136bd7182de4138cbb1b611a2
Artem Yavorskyi <aqson@me.com>
2025-04-17 08:48:22 +0000

    feat: add raygun error detection tool

--- packages/features/src/categories/error-tracking/patterns/index.ts
+++ packages/features/src/categories/error-tracking/patterns/index.ts
@@ -1,7 +1,9 @@
 import { sentry } from './sentry.js';
 import { rollbar } from './rollbar.js';
+import { raygun } from './raygun.js';
 
 export const patterns = {
   sentry,
   rollbar,
+  raygun,
 } as const;
